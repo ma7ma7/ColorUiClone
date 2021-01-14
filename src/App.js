79a08fb61bb771c5c3a0c6a4973ed401539ec6e7
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-01-14 20:55:35 +0100

    Add chroma-js/ colorHelpers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Pallete from "./Palette.js";
 import seedColors from "./seedColors.js";
+import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]));
     return <Pallete {...seedColors[4]} />;
   }
 }
