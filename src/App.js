30b175d974eb23dbe9e799600b0769cec17918da
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-01-15 15:49:44 +0100

    Adding React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Pallete from "./Palette.js";
 import seedColors from "./seedColors.js";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
-    return <Pallete palette={generatePalette(seedColors[4])} />;
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>Pallet List</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>Pallet Colors</h1>}
+        />
+      </Switch>
+      // <Pallete palette={generatePalette(seedColors[4])} />;
+    );
   }
 }
 
