c87cdbe998f9e791162f2756a51c29dfe2a1814e
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-01-15 11:21:17 +0100

    Add Material-ui/Select MenuItem

--- src/Palette.js
+++ src/Palette.js
@@ -6,24 +6,33 @@ import "./Palette.css";
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
   }
 
+  changeFormat(format) {
+    this.setState({ format });
+  }
+
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
 
     const colorBoxs = colors[level].map((color) => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ));
     return (
       <div class="Palette">
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         <div className="Palette-colors">{colorBoxs}</div>
         {/* footer goes here */}
       </div>
