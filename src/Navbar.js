a4f74dc2f516f64b03f964a89409cf10976fce5b
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-01-15 14:21:01 +0100

    Add Snackbar/Material-ui Icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,25 +2,32 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import SnackBar from "@material-ui/core/SnackBar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { format: "hex" };
+    this.state = { format: "hex", open: false };
     this.handleChange = this.handleChange.bind(this);
+    this.handleCloseSnackBare = this.handleCloseSnackBare.bind(this);
   }
 
   handleChange(e) {
-    this.setState({ format: e.target.value }, () => {
-      this.props.handleChange(e.target.value);
-    });
+    this.setState({ format: e.target.value, open: true });
+    this.props.handleChange(e.target.value);
+  }
+
+  handleCloseSnackBare() {
+    this.setState({ open: false });
   }
 
   render() {
     const { level, changeLevel } = this.props;
-    const { format } = this.state;
+    const { format, open } = this.state;
 
     return (
       <header className="Navbar">
@@ -46,6 +53,25 @@ class Navbar extends Component {
             <MenuItem value="rgba">RGBA - (255,255,255, 1.0)</MenuItem>
           </Select>
         </div>
+
+        {/* The Snack Bar Material-ui Component */}
+        <SnackBar
+          onClose={this.handleCloseSnackBare}
+          open={open}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          autoHideDuration={3000}
+          message={`Format Changed to ${format.toUpperCase()}`}
+          action={
+            <React.Fragment>
+              <IconButton color="inherit">
+                <CloseIcon
+                  fontSize="small"
+                  onClick={this.handleCloseSnackBare}
+                />
+              </IconButton>
+            </React.Fragment>
+          }
+        />
       </header>
     );
   }
